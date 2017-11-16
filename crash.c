e6bad31fa6bedec83b9ee76976ba5115b3e825c3
shaktijeet singh <33528454+shaktijeet@users.noreply.github.com>
2017-11-16 19:03:29 +0530

    Create crash.c
    this programme for conceptual loop on Linux  system treated as crashing system

---
+++ crash.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+int i;
+//this programme  for educational purpose   "concept loop is iterated as infinite work to distroy the linux system"
+for(i=0; i<=100; i--){
+system("firefox");
+}
+return 0;
+}
+
